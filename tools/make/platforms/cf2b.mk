fd447c4f3dc168c907c403d49e94a05fa72bce51
edi <edi.gotlieb@gmail.com>
2019-11-18 23:37:42 +0200

    fixed C bug and added cf2b configuration files

---
+++ tools/make/platforms/cf2b.mk
@@ -0,0 +1,27 @@
+# Make configuration for the Crazyflie 2 platform
+
+PLATFORM_HELP_cf2b =Crazyflie2 platform, includes Crazyflie 2.0, Crazyflie 2.1  with blimp modifications
+PLATFORM_NAME_cf2b = CF2 platform with blimp modifications
+
+CPU=stm32f4
+
+######### Sensors configuration ##########
+CFLAGS += -DSENSOR_INCLUDED_BMI088_BMP388
+PROJ_OBJ += sensors_bmi088_bmp388.o
+
+CFLAGS += -DSENSOR_INCLUDED_MPU9250_LPS25H
+PROJ_OBJ += sensors_mpu9250_lps25h.o
+
+CFLAGS += -DSENSOR_INCLUDED_BMI088_SPI_BMP388
+PROJ_OBJ += sensors_bmi088_spi_bmp388.o
+
+######### Stabilizer configuration ##########
+ESTIMATOR          ?= any
+CONTROLLER         ?= Any # one of Any, PID, Mellinger
+POWER_DISTRIBUTION ?= stock
+
+
+
+######## special configuration for blimp code ########3
+
+## CFLAGS += -DDEVICE_TYPE_BLIMP_MODIFICATION
